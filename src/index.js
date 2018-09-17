85c76f7081e677b42b286f4e27d0e75709c6134d
DimaBog22 <bogdanov111222@yandex.ru>
2018-09-17 15:04:14 +0300

    Create class Sorter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,51 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.arr = [];
+        this.compareFunction = function(a,b) {
+            return a - b;
+        }; 
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.arr.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.arr[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        let arrLength = this.arr.length;
+        return arrLength;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.arr;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        let newArr = [];
+        let IndicesLength = indices.length;
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        for (let i = 0; i < IndicesLength; i++) {
+            newArr.push(this.arr[indices[i]]);
+        }
+        
+        indices.sort(function(a,b) {
+            return a - b;
+        });
+        
+        newArr.sort(this.compareFunction);
+
+        for (let j = 0; j < IndicesLength; j++) {
+            this.arr[indices[j]] = newArr[j];
+        }
+      
+    }
+
+    setComparator(compareFunction) {
+        this.compareFunction = compareFunction;
+    }
 }
 
 module.exports = Sorter;
